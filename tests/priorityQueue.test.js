ee0193822b98cc3c979336c8e2dca18beb3e6a53
CadenceRiddle <cadence475@gmail.com>
2024-08-25 08:37:59 -0400

    finished test cases for the methods

--- tests/priorityQueue.test.js
+++ tests/priorityQueue.test.js
@@ -4,3 +4,16 @@ test('check to make sure the priority queue is created', () => {
   const temp = new PriorityQueue();
   expect(temp).toBeTruthy();
 });
+
+test('check to see if element is enqueued', () => {
+  const temp = new PriorityQueue();
+  temp.enqueue("A", 5);
+  expect(temp.isEmpty()).toBeFalsy();
+});
+
+test('check to see if element is dequeued', () => {
+  const temp = new PriorityQueue();
+  temp.enqueue("A", 5);
+  temp.dequeue();
+  expect(temp.isEmpty()).toBeTruthy();
+})
