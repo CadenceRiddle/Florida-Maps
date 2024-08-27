99c31e8e12a7efdfdace4fa0106811599aa11226
CadenceRiddle <cadence475@gmail.com>
2024-08-27 12:32:56 -0400

    modified code to export sequelize and location

--- src/sequelize.js
+++ src/sequelize.js
@@ -1,39 +1,31 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('florida-maps', 'postgres', 'Gallego$0908', {
+const sequelize = new Sequelize('florida-maps', 'postgres', 'Ga11ego$0908', {
   dialect: 'postgres'
 });
 
-sequelize.authenticate().then(() => {
-  console.log('connected successfully to database')
-}).catch((err)=>{
-  console.log(err);
-})
-
-const Locations = sequelize.define('locations', {
+const Location = sequelize.define('locations', {
   locationID: {
-      type: Sequelize.DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
+    type: Sequelize.DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
   },
-
   locationName: {
     type: Sequelize.DataTypes.STRING,
     allowNull: false
   },
-
-  neighbors : {
+  neighbors: {
     type: Sequelize.DataTypes.JSON,
     allowNull: false
   }
 });
 
-Locations.sync({alter: true}).then(() => {
+sequelize.sync({ alter: true }).then(() => {
   console.log('Synced to database successfully');
 }).catch((err) => {
   console.log('Failed to sync to database');
-});
+});;
 
 module.exports = {
-  sequelize, 
-  Locations
-}
+  sequelize,
+  Location
+};
