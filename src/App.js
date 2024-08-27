f417bede9fe449f238695d23a19529f9c18c051c
CadenceRiddle <cadence475@gmail.com>
2024-08-27 12:35:09 -0400

    imported router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const locations_route = require('./locations_route');
+const { sequelize } = require('./sequelize');
+const session = require('express-session');
 
-app.listen(PORT, ()=>{
-  console.log(`Listening on port ${PORT}`);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sessionStore = new SequelizeStore({ db: sequelize });
+
+app.use('/api', locations_route)
+
+
+app.listen(PORT, async ()=>{
+ await console.log(`Listening on port ${PORT}`);
 })
 
-
+app.get('/', (req, res, next)=>{
+  res.status(200).send("Hello world");
+})
