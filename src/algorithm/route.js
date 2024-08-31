e19d84f2556853448abf8707d9f3824f46f84ac7
CadenceRiddle <cadence475@gmail.com>
2024-08-31 00:12:00 -0400

    created first version of the algorithm route

--- src/algorithm/route.js
+++ src/algorithm/route.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const dijkstra = require('./shortestPath')
+const dijkstra = require('./shortestPath');
+const graphify = require('./graphify');
 
 const createAlgorithmRouter = (database) =>{
   const router = express.Router();
@@ -10,13 +11,22 @@ const createAlgorithmRouter = (database) =>{
     if (!locationA || !locationB) {
         return res.status(400).json({ error: 'both locations are required' });
     };
+    try{
+      const targetA = await database.findOne({
+        locationName: locationA
+      });
+      const targetB = await database.findOne({
+        locationName: locationB
+      });
 
-    const targetA = await database.findOne({
-      locationName: locationA
-    });
-    const targetB = await database.findOne({
-      locationName: locationB
-    });
+      const graph = graphify(database);
+      const result = dijkstra(graph, targetA, targetB);
+
+      res.status(200).json({ path: result });
+    }
+    catch(err){
+      res.status(400).json({ error: err.message });
+    }
   })
 
   return router;
