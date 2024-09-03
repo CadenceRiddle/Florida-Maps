7e0523d26f062aaa1f1b08d8de60ca36a522b7dd
CadenceRiddle <cadence475@gmail.com>
2024-09-03 11:50:13 -0400

    added further testing cases

--- tests/shortestPath.test.js
+++ tests/shortestPath.test.js
@@ -11,4 +11,22 @@ test('check to see if dijkstra runs correctly', () => {
   let path = dijkstra(graph, 'A', 'D');
 
   expect(path).toEqual(['A', 'B', 'C', 'D'])
+});
+
+test('check to see if dijkstra runs correctly on a larger scale', () => {
+  const graph = {
+    '1': {'6': 3, '3': 4},
+    '2': {'6': 2},
+    '3': {'1': 1, '4': 4},
+    '4': {'3': 4, '5': 5, '7': 6},
+    '5': {'10': 8, '6': 7, '4': 5, '9': 2},
+    '6': {'2': 2, '1': 1, '5': 7},
+    '7': {'8': 3, '4': 4},
+    '8': {'9': 4, '7': 3},
+    '9': {'5': 2, '8': 4},
+    '10': {'5': 8},
+  };
+  
+  let path = dijkstra(graph, '1', '8');
+  expect(path).toEqual(['1', '6', '5', '9', '8']);
 })
